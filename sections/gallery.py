f09f40e90bc0f95ae9b139e6eb54feb43b743991
Jigar Patel <astrojigs24@gmail.com>
2025-08-29 14:27:38 +0100

    added manim example

--- sections/gallery.py
+++ sections/gallery.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from core.utils import custom_container, custom_write
+from core.utils import custom_container, custom_write, hero_video
 from core.examples import dublin_proximity_gis, complex_radar_chart
 
 
@@ -21,9 +21,11 @@ def render():
         with custom_container(key='complex radar chart'):
             complex_radar_chart.render()
 
-    with c1:
-        # Manim example
-        pass
+    # Manim example
+    # with custom_container(key="Manim example", bg="#f4f3ed"):
+    custom_write("Physics - Superposition in Motion", color='gray', type='h2')
+    hero_video(path="./core/references/gifs/Manim Example.mp4",
+               mp4_path="./core/references/gifs/Manim Example.mp4")
 
     # more example coming in
-    custom_write('More examples coming in...',type='h4', color='gray')
+    custom_write('More examples coming in...', type='h4', color='gray')

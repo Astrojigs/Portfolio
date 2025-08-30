594b82c2d847ce14f0cd06ea8b2c96ba3fc1b221
Jigar Patel <astrojigs24@gmail.com>
2025-08-30 14:11:41 +0100

    added reinforcement learning example

--- sections/gallery.py
+++ sections/gallery.py
@@ -4,32 +4,40 @@ from core.examples import dublin_proximity_gis, complex_radar_chart
 
 
 def render():
-    custom_write('Gallery', type='h2', color='gray')
+    # custom_write('Gallery', type='h2', color='gray')
 
-    # caption
-    custom_write("Here are some examples of what I do", color='gray', type='caption')
-
-    c1, c2 = st.columns([1, 1.2])
-
+    c1, c2 = st.columns([3, 4], vertical_alignment='top', border=False)
     with c1:
-        # GIS example
-        with custom_container(key='GIS example', hover_lift_px=3, radius="60px", padding="20px"):
-            dublin_proximity_gis.render()
-
+        # Title
+        st.write("# Gallery")
+        st.caption("Here are some examples of what I do")
     with c2:
-        # Echarts example
-        with custom_container(key='complex radar chart',hover_lift_px=3, radius="100px", padding="40px"):
-            complex_radar_chart.render()
-
-    c1, c2 = st.columns([1.5,1])
-    with c1:
-        # Manim example (website background color #f4f3ed)
-        with custom_container(key="Manim example", bg="#ffffff",hover_lift_px=3, radius="40px", padding="40px"):
+        with custom_container(key="Manim example", bg="#ffffff", hover_lift_px=3, radius="20px", padding="40px"):
             custom_write("Physics - Superposition in Motion", color='gray', type='h4')
             custom_write("Made using <i><b>Manim<b/><i/>", color='gray', type='caption')
 
             hero_video(path="./core/references/gifs/Manim Example.mp4",
                        mp4_path="./core/references/gifs/Manim Example.mp4")
+
+        st.caption(':red[**Note**]: *Some **Gallery** examples use :blue[dummy data] and do not represent '
+                   'real world statistics.*')
+
+    with c1:
+        # GIS example
+        with custom_container(key='GIS example', hover_lift_px=3, radius="30px", padding="10px"):
+            dublin_proximity_gis.render()
+
+    with c2:
+        # Echarts example
+        with custom_container(key='complex radar chart', hover_lift_px=3, radius="100px", padding="40px"):
+            complex_radar_chart.render()
+
+    with c1:
+        with custom_container(key='rl example', hover_lift_px=3, radius="20px", padding="10px"):
+            rl_repo_link = "https://github.com/Astrojigs/LunarLander-Agent"
+            custom_write("Reinforcement Learning - Lunar Lander", color='gray', type='h4')
+            st.page_link(rl_repo_link, label="🪄 :red[Link to Repo]", icon=":material/visibility:")
+            st.image("./core/references/gifs/reinforcement learning example.gif")
 
     # more example coming in
     custom_write('More examples coming in...', type='h4', color='gray')

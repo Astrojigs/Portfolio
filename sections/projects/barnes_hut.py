05b95c83daff423b2938a68f3096759fd2b91d3a
Jigar Patel <astrojigs24@gmail.com>
2025-08-30 18:22:50 +0100

    Added Barnes Hut GIFs

--- sections/projects/barnes_hut.py
+++ sections/projects/barnes_hut.py
@@ -1,7 +1,17 @@
 import streamlit as st
-from core.utils import custom_container, custom_write, chips
+from core.utils import custom_container, custom_write, chips, hero_video
 
 
 def render():
     custom_write('Barnes Hut Algorithms', color="orange", type='h2')
+
+    with custom_container(key="Barnes-Hut Example"):
+        c1, c2 = st.columns([1, 1])
+        with c1:
+            hero_video("./core/references/Project Files/Barnes-Hut/ep2_web.mp4",
+                       mp4_path="./core/references/Project Files/Barnes-Hut/ep2_web.mp4")
+        with c2:
+            hero_video('./core/references/Project Files/Barnes-Hut/ep2_quadtree_web.mp4',
+                       mp4_path="./core/references/Project Files/Barnes-Hut/ep2_quadtree_web.mp4")
+
     custom_write("<i>More details coming in soon</i>...", color='gray')
